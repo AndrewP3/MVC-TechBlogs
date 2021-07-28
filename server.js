ff1e28aaa42ec6f9928b9bb2878a6228891d718b
AndrewP3 <andrewpowell097@gmail.com>
2021-07-28 15:52:54 -0700

    1st commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const sequelize = require("./connection");
+const helpers = require("./helpers");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const hbs = exphbs.create({ helpers });
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
